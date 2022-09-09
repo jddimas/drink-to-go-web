e221505c355de1b539e1c92e3bc86e34b9574cf9
ddimasrg <jdimasamador@gmail.com>
2022-09-09 03:03:56 -0500

    typo test root

--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,7 @@
 ==========================================================================================*/
 const path = require('path');
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/drink-to-go-web/'
-    : '/',
+  publicPath: '/',
   configureWebpack: {
     resolve: {
       alias: {
